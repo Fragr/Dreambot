1dffb8fcfcc721799840d5ec0068fccc7fce9aa6
Unknown <p.j.debisschop@gmail.com>
2018-04-29 11:29:46 -0500

    Initial commit

---
+++ WildyAlcher/src/org/dreambot/fragr/Main.java
@@ -0,0 +1,30 @@
+package org.dreambot.fragr;
+
+import org.dreambot.api.script.AbstractScript;
+
+public class Main extends AbstractScript {
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public int onLoop() {
+        return 0;
+    }
+
+    @Override
+    public void onExit() {
+        super.onExit();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+}
---
+++ WildyAlcher/src/org/dreambot/fragr/api/GUI.java
@@ -0,0 +1,6 @@
+package org.dreambot.fragr.api;
+
+import javax.swing.*;
+
+public class GUI extends JFrame {
+}
