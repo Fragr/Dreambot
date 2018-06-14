d3feebbee1a3a546258cd26acc9ae30c9b92e3ca
Unknown <p.j.debisschop@gmail.com>
2018-06-14 01:16:09 -0500

    Began work on gold bracelet crafter

---
+++ fGoldCrafter/src/org/dreambot/fragr/GC.java
@@ -0,0 +1,20 @@
+package org.dreambot.fragr;
+
+import org.dreambot.api.script.Category;
+import org.dreambot.api.script.ScriptManifest;
+import org.dreambot.api.script.impl.TaskScript;
+
+import java.awt.*;
+
+@ScriptManifest(author = "Fragr", category = Category.CRAFTING, description = "Crafts bracelets from gold bars", name = "fGoldCrafter", version = 1.0)
+public class GC extends TaskScript {
+    @Override
+    public void onStart() {
+
+    }
+
+    @Override
+    public void onPaint(Graphics graphics) {
+
+    }
+}
